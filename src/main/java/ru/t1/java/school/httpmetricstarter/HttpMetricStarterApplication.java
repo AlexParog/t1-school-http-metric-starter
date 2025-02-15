9a1a7fc62a309822f0ac9607ed0a30dfad14f26a
Alex Parog <sbibik2000@mail.ru>
2025-02-15 18:51:11 +0300

    feat: init project

---
+++ src/main/java/ru/t1/java/school/httpmetricstarter/HttpMetricStarterApplication.java
@@ -0,0 +1,13 @@
+package ru.t1.java.school.httpmetricstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HttpMetricStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HttpMetricStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/t1/java/school/httpmetricstarter/HttpMetricStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.t1.java.school.httpmetricstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpMetricStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
